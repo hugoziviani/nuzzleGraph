1c366780623891cce10c4e6f380417afe4510b21
hugo.ziviani <hugoziviani@gmail.com>
2019-05-03 21:46:46 -0300

    Versao final nozzle

--- hsvToGray.py
+++ hsvToGray.py
@@ -3,7 +3,6 @@ import cv2
 import math
 from matplotlib import pyplot as plt
 
-
 def plotaComGraph (v1, threshIm):
     titles = ['Original','Thresh']
     images = [v1, threshIm]
@@ -13,64 +12,156 @@ def plotaComGraph (v1, threshIm):
         plt.xticks([]),plt.yticks([])
     plt.show()
 
-#cap = cv2.VideoCapture(0)
-cap = cv2.imread('/Users/hz/Documents/nuzzlePy/tubos2.png')
-heigth, width, channels = img.shape
-print(heigth)
-print(width)
+cap = cv2.imread('/Users/hz/Documents/nozzlePy/tubos2.png')
+high, width, channels = cap.shape
 
-#while(True):
+print("Altura:",high)
+print("Largura:",width)
+# desenho p1 (0,0) p2(raio+i,
 
-#ret, frame = cap.read()
 frame = cap
 hsv = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
-
-#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 h, s, v1 = cv2.split(hsv)
-bilateral_filtered_image = cv2.bilateralFilter(v1, 5, 175, 175)
-#cv2.imshow('Bilateral', bilateral_filtered_image)
-edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
-#cv2.imshow('Edge', edge_detected_image)
-contours, a1 = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-xP2 = []
-contour_list = []
-for contour in contours:
-    approx = cv2.approxPolyDP(contour,0.025*cv2.arcLength(contour,True),True)
-    area = cv2.contourArea(contour)
-    if ((len(approx) > 8) & (area > 30) ):
-        xP2 = float(area)/3.14
-        contour_list.append(contour)
-cv2.drawContours(frame, contour_list,  -1, (0,255,0), 1)
-
-crop_img = []
-y = 0
-x = int(xP2)
-h = 444
-w = 10
-for tubo in range(61):
-    cv2.rectangle(frame,(0,0),(int(x)*int(tubo), h),(0,255,0),1)
-    crop_img = v1[y:y+h*tubo, x:x+w]
-cv2.imshow('Objects Detected', frame)
-cv2.imshow('Croped!', crop_img)
-
+blur = cv2.medianBlur(v1,7)
+cv2.imshow('Blur', blur)
 
 lower_black = np.array([0])
-upper_black = np.array([30])
-mask = cv2.inRange(bilateral_filtered_image, lower_black, upper_black)
-ret2, threshIm = cv2.threshold(mask, 50, 255,cv2.THRESH_BINARY)
+upper_black = np.array([35])
+mask = cv2.inRange(blur, lower_black, upper_black)
+ret2, threshIm = cv2.threshold(mask, 34, 255,cv2.THRESH_BINARY)
+
+
+kernelO = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+opening = cv2.morphologyEx(threshIm, cv2.MORPH_OPEN, kernelO)
+#cv2.imshow('Opening', opening)
+
+
+kernelE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
+erosion = cv2.erode(opening,kernelE,iterations = 3)
+#cv2.imshow('Erosion', erosion)
+
+kernelC = cv2.getStructuringElement(cv2.MORPH_CROSS,(2,2))
+dilationC = cv2.dilate(opening,kernelO,iterations = 2)
+#cv2.imshow('DilatadaC', dilationC)
+
+kernelO = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
+dilation = cv2.dilate(opening,kernelO,iterations = 4)
+cv2.imshow('Dilatada', dilation)
+
+contornos = dilation
+bilateral_filtered_image = cv2.bilateralFilter(contornos, 5, 175, 175)
+edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+contours, a1 = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#contours - é um vetor com os contornos
+#o contorno é um vetor com os pontos
+contour_list = []
+area_list = []
+
+
+for contour in contours:
+    approx = cv2.approxPolyDP(contour,0.010*cv2.arcLength(contour,True),True)
+    area = cv2.contourArea(contour)
+    #print("contorno: ",contour)
+    if ((len(approx) > 8) & (area > 30)):
+        area_list.append(area)
+        contour_list.append(contour)
+
+# varrer os contornos selecionados buscando a menor e a maior area de contorno.
+print ("Area tamanho:",len(area_list))
+print ("Contornos tamanho:",len(contour_list))
+
+#print(contours)
+longeDis = 0.0
+pertoDis = 0.0
+distA = 0
+distB = 0
 
 
 
 
-cv2.imshow('trash', threshIm)
+i = 0
+for contorno in contour_list:
+    xP = 0
+    yP = 0
+    xL = 0
+    yL = 0
+    for pto in range (len(contorno)):
+        xA = 0
+        yA = 0
+        xB = contorno.item(pto,0,0)
+        yB = contorno.item(pto,0,1)
+
+        distAB = math.sqrt(((xA-xB)**2) + ((yA-yB)**2))
+        print (distAB)
+        if(pto==0):
+            longeDis = distAB
+            pertoDis = distAB
+        if(pertoDis < distAB):
+            pertoDis = distAB
+            xP = int(contorno.item(pto,0,0))
+            yP = int(contorno.item(pto,0,1))
+            print("xP:",xP,"yP",yP)
+            
+        if(longeDis > distAB):
+            longeDis = distAB
+            xL = int(contorno.item(pto,0,0))
+            yL = int(contorno.item(pto,0,1))
+            print("xL:",xP,"yL",yP)
+    
+        cv2.rectangle(frame,(xP,0),(xL, high),(0,255,0),1)
+
+    print(i)
+    i = i+1
+
+
+#varrer os contornos
+#pegar o pto mais distante e o mais próximo da origem
+
+
+
+# media das areas para cortar em setores a figura
+#setor = math.sqrt(maiorArea/math.pi)
+#enaquato houver foto ele vai dividir pelo tamanho do setor.
+
+
+cv2.drawContours(frame, contour_list, -1, (0,255,0), 1)
+cv2.imshow("comContornos", frame)
+
+
+
+
+
+
+#Criar um ponto auxiliar que recebe o maior e o menor do contorno e setar na lista para ser desenhada as linhas
+#cv2.line(frame,(menorP[0],menorP[1]),(maiorP[0],maiorP[1]),(255,0,0),1)
+
+                        #print (distAB)
+
+
+
+#print("Menor: ",menorP)
+#print("Maior: ",maiorP)
+#cv2.line(frame, menorP,  -1, (255,0,0), 1)
+#cv2.line(frame, maiorD,  -1, (0,0,255), 1)
+
+
+
+
+
+
+#for area in area_list:
+#print(math.sqrt(float(area))*3.14)
+#cv2.imshow('Objects Detected', frame)
+
+#for tubo in range(61):
+#cv2.rectangle(frame,(0,0),(int(x)*int(tubo), h),(0,255,0),1)
+#cv2.imshow('Objects Detected', frame)
+
+
 #cv2.imshow('gray!', v1)
-#plotaComGraph(v1,threshIm)
-#if cv2.waitKey(1) & 0xFF == ord('q'):
-#   break
+#cv2.imshow('Thrash!', threshIm)
 cv2.waitKey(0)
-# When everything done, release the capture
-cap.release()
 cv2.destroyAllWindows()
 
 
--- nozzle1.py
+++ nozzle1.py
@@ -1,28 +1,147 @@
 import numpy as np
 import cv2
+import math
+import imutils
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+def plotaComGraph (v1, threshIm):
+    titles = ['Original','Thresh']
+    images = [v1, threshIm]
+    for i in range(len(images)):
+        plt.subplot(1,2,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # Our operations on the frame come here
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #gray = cv2.cvtColor(hsv, cv2.COLOR_HSV2GRAY)
-    ret2, trashIm = cv2.threshold(hsv,127,255,cv2.THRESH_BINARY)
+cap = cv2.imread('/Users/hz/Documents/nozzlePy/tubos2.png')
+high, width, channels = cap.shape
 
-    lower_black = np.array([0,0,0])
-    upper_black = np.array([0,0,10])
-    mask = cv2.inRange(trashIm, lower_black, upper_black)
-    
-    
-    # Display the resulting frame
-    #cv2.imshow('cinza',gray)
-    #cv2.imshow('original',frame)
-    cv2.imshow('mask', mask)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+print("Altura:",high)
+print("Largura:",width)
+# desenho p1 (0,0) p2(raio+i,
 
-# When everything done, release the capture
-cap.release()
+frame = cap
+hsv = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
+h, s, v1 = cv2.split(hsv)
+
+blur = cv2.medianBlur(v1,7)
+cv2.imshow('Blur', blur)
+
+lower_black = np.array([0])
+upper_black = np.array([35])
+mask = cv2.inRange(blur, lower_black, upper_black)
+ret2, threshIm = cv2.threshold(mask, 34, 255,cv2.THRESH_BINARY)
+
+kernelO = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+opening = cv2.morphologyEx(threshIm, cv2.MORPH_OPEN, kernelO)
+#cv2.imshow('Opening', opening)
+
+kernelE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,2))
+erosion = cv2.erode(opening,kernelE,iterations = 3)
+#cv2.imshow('Erosion', erosion)
+
+kernelC = cv2.getStructuringElement(cv2.MORPH_CROSS,(2,2))
+dilationC = cv2.dilate(opening,kernelO,iterations = 2)
+#cv2.imshow('DilatadaC', dilationC)
+
+kernelO = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
+dilation = cv2.dilate(opening,kernelO,iterations = 4)
+cv2.imshow('Dilatada', dilation)
+
+contornos = dilation
+bilateral_filtered_image = cv2.bilateralFilter(contornos, 5, 175, 175)
+edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+contours, a1 = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#contours - é um vetor com os contornos
+#o contorno é um vetor com os pontos
+contour_list = []
+area_list = []
+
+
+for contour in contours:
+    approx = cv2.approxPolyDP(contour,0.009*cv2.arcLength(contour,True),True)
+    area = cv2.contourArea(contour)
+    #print("contorno: ",contour)
+    if ((len(approx) > 8) & (area > 30)):
+        area_list.append(area)
+        contour_list.append(contour)
+
+# varrer os contornos selecionados buscando a menor e a maior area de contorno.
+print ("Area tamanho:",len(area_list))
+print ("Contornos tamanho:",len(contour_list))
+
+#print(contours)
+cXAnt = 0
+cYAnt = 0
+ptosVetor = []
+for contorno in range (len(contour_list)):
+    M = cv2.moments(contour_list[contorno])
+    cX = int(M["m10"] / M["m00"])
+    cXAnt = cX
+    cY = int(M["m01"] / M["m00"])
+    cYAnt = cY
+    ptosVetor.append([cX, cY])
+
+#print(contorno,"-:","cX:", cX, "cY:", cY)
+print ("Ptos-SO",ptosVetor)
+ptosVetor.sort()
+print ("Ptos",ptosVetor)
+
+for pto in range (len(ptosVetor)):
+    cv2.circle(frame, (ptosVetor[pto][0], ptosVetor[pto][1]), 7, (255, 0, 0), -1)
+    per = ((high - ptosVetor[pto][1]) / high)
+    if(pto%2 == 0):
+        print ("Tubo-",pto," ", math.fabs(per)*100,"%")
+
+
+#varrer os contornos
+#pegar o pto mais distante e o mais próximo da origem
+
+
+
+# media das areas para cortar em setores a figura
+#setor = math.sqrt(maiorArea/math.pi)
+#enaquato houver foto ele vai dividir pelo tamanho do setor.
+
+
+cv2.drawContours(frame, contour_list, -1, (0,255,0), 1)
+cv2.imshow("comContornos", frame)
+
+
+
+
+
+
+#Criar um ponto auxiliar que recebe o maior e o menor do contorno e setar na lista para ser desenhada as linhas
+#cv2.line(frame,(menorP[0],menorP[1]),(maiorP[0],maiorP[1]),(255,0,0),1)
+
+#print (distAB)
+
+
+
+#print("Menor: ",menorP)
+#print("Maior: ",maiorP)
+#cv2.line(frame, menorP,  -1, (255,0,0), 1)
+#cv2.line(frame, maiorD,  -1, (0,0,255), 1)
+
+
+
+
+
+
+#for area in area_list:
+#print(math.sqrt(float(area))*3.14)
+#cv2.imshow('Objects Detected', frame)
+
+#for tubo in range(61):
+#cv2.rectangle(frame,(0,0),(int(x)*int(tubo), h),(0,255,0),1)
+#cv2.imshow('Objects Detected', frame)
+
+
+#cv2.imshow('gray!', v1)
+#cv2.imshow('Thrash!', threshIm)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+

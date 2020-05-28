9657ccd095d5b664fa908f7bd92e0aea4c651624
hz <hz@hzs-MacBook-Air.local>
2020-05-28 14:23:44 -0300

    refactoring code

---
+++ config.py
@@ -0,0 +1,16 @@
+# constants to image process
+
+AREA_BALLS_SENSIBILITY = 0.1
+DRAW_LINES_THICKNES = 1
+
+
+# limiar blackness to detect the limit of contours on canny
+KERNEL_SIZE_CANNY = 3
+CANNY_DOWN_THRESHOLD = 0
+CANNY_UPPER_THRESHOLD = 60
+
+
+PURE_RED = (255, 0, 0)
+PURE_GREEN = (0, 255, 0)
+PURE_BLUE = (0, 0, 255)
+
